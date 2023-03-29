de4ec8bc7bb38825ea8b19ff482205f77a9204b1
AntonGraf <ant0ngraf@yandex.ru>
2023-03-29 07:32:47 +0600

    Initial commit

---
+++ src/test/java/ru/skypro/sockswarehouse/SocksWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.sockswarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocksWarehouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
